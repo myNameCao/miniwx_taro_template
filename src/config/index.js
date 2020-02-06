6694d211ca67fbf9b182ee6e4e07770ada36c384
曹合飞 <caohefei@gokuaidian.com>
2020-02-06 22:25:45 +0800

    add

--- src/config/index.js
+++ src/config/index.js
@@ -1,28 +1,24 @@
 
 export default {
   czb_api: {
-    platformType: "10088001",
+    platformType: "",
     production: {
-      appKey: "fl_prod_mp",
-      appId: "wx08061489992067f3",
-      appSecret: "00d9f9637625a46fdb6861d79d207cdf",
-      host: "https://mpcs.fleetingpower.com"
+      appKey: "",
+      appId: "",
+      appSecret: "",
+      host: ""
     },
     development: {
-      appKey: "fl_test_mp",
-      appId: "wx08061489992067f3",
-      appSecret: "00d9f9637625a46fdb6861d79d207cdf",
-      host: "127.0.0.1"
+      appKey: "",
+      appId: "",
+      appSecret: "",
+      host: ""
     },
     path: {
-      getTelCode: "/services/v3/begin/commonSendMsg", //  get phone code
-      signUp: "/services/v3/begin/fuliWechatMPRegisterLogin", // login
-      signIn: "/services/v3/begin/fuliWechatMPLogin", // signIn
-      deatilActivity:"/services/v3/pullNewActivity/activityDetailsPage",
-      queryActivityList: "/services/v3/pullNewActivity/queryActivityList",
-      startActive: "/services/v3/pullNewActivity/startInvite",
-      shareMessage: "/services/v3/pullNewActivity/shareInfo",
-      assistInvite: "/services/v3/pullNewActivity/assistInvite",
+      getTelCode: "", //  get phone code
+      signUp: "", // login
+      signIn: "", // signIn
+      
     }
   },
   czb_public: {
